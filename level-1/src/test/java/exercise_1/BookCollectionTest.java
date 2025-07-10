e79ac885a9d8ee24b0259550249fd8bc81e5449f
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-10 11:22:07 +0200

    feat: create test for BookCollection constructor

---
+++ level-1/src/test/java/exercise_1/BookCollectionTest.java
@@ -0,0 +1,15 @@
+package exercise_1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class BookCollectionTest {
+
+    @Test
+    public void BookCollectionTest() {
+        BookCollection bookCollection = new BookCollection();
+        assertNotNull(bookCollection);
+    }
+
+}
