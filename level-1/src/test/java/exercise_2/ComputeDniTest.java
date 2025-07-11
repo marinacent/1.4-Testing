ebbc1a32491731e3abf1c6c13add462984ab70cf
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-11 12:55:15 +0200

    feat: write test to check that the dni letter is computed correctly

---
+++ level-1/src/test/java/exercise_2/ComputeDniTest.java
@@ -0,0 +1,30 @@
+package exercise_2;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ComputeDniTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "87654321,  X",
+            "43218765,  V",
+            "56789012,  B",
+            "34567890,  V",
+            "78901234,  X",
+            "21098765,  Z",
+            "12345678,  Z",
+            "78714688,  V",
+            "77529779,  E",
+            "91142657,  M"
+    })
+    void when_givenDniNumber_then_computeCorrectLetter(String number, String letter) {
+        int intNumber = Integer.valueOf(number);
+        String returnedLetter = ComputeDni.getLetter(intNumber);
+        assertEquals(letter, returnedLetter);
+    }
+
+}
