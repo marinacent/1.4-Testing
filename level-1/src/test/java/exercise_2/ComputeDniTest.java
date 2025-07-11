dc3cddd6c408cfcafd5bc630b9a9ab118623ff22
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-11 21:27:19 +0200

    fix[ComputeDniTest]: remove spaces from CsvSource

--- level-1/src/test/java/exercise_2/ComputeDniTest.java
+++ level-1/src/test/java/exercise_2/ComputeDniTest.java
@@ -10,16 +10,16 @@ public class ComputeDniTest {
 
     @ParameterizedTest
     @CsvSource({
-            "87654321,  X",
-            "43218765,  V",
-            "56789012,  B",
-            "34567890,  V",
-            "78901234,  X",
-            "21098765,  Z",
-            "12345678,  Z",
-            "78714688,  V",
-            "77529779,  E",
-            "91142657,  M"
+            "87654321,X",
+            "43218765,V",
+            "56789012,B",
+            "34567890,V",
+            "78901234,X",
+            "21098765,Z",
+            "12345678,Z",
+            "78714688,V",
+            "77529779,E",
+            "91142657,M"
     })
     void when_givenDniNumber_then_computeCorrectLetter(String number, String letter) {
         int intNumber = Integer.parseInt(number);
