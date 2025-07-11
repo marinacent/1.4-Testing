69e1da57ceb12be53be2d756496715d0569c47be
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-11 21:46:28 +0200

    refactor: add index to exception message

--- level-1/src/main/java/exercise_3/IndexOutOfBounds.java
+++ level-1/src/main/java/exercise_3/IndexOutOfBounds.java
@@ -4,7 +4,7 @@ public class IndexOutOfBounds {
 
     public static char getCharByIndex(char[] charArray, int index) {
         if (index < 0 || index >= charArray.length) {
-            throw new ArrayIndexOutOfBoundsException("Invalid index");
+            throw new ArrayIndexOutOfBoundsException("Invalid index: " + index);
         }
         return charArray[index];
     }
