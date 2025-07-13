df67adbae7d941052e1802ca29609e6ebcf30bda
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-13 04:01:38 +0200

    test: test map without redundant class

--- level-2/src/test/java/level2_exercises/IntGroupingTest.java
+++ level-2/src/test/java/level2_exercises/IntGroupingTest.java
@@ -65,3 +65,4 @@ public class IntGroupingTest {
         }).isInstanceOf(ArrayIndexOutOfBoundsException.class);
 
     }
+}
--- level-2/src/test/java/level2_exercises/StringMapTest.java
+++ level-2/src/test/java/level2_exercises/StringMapTest.java
@@ -2,17 +2,18 @@ package level2_exercises;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringMapTest {
 
     @Test
     void when_addingElementToMap_then_keyInMap () {
-        String key = "key";
-        String value = "value";
-        StringMap testMap = new StringMap();
-        testMap.addElement(key, value);
-        assertThat(testMap.getMap().keySet()).contains(key);
+        Map<String,String> stringMap = new HashMap<>();
+        stringMap.put("key", "value");
+        assertThat(stringMap.keySet()).contains("key"));
     }
 
 }
